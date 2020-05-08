51fefe4e1e028faae386999b806f5e07e5b05129
Jamison Cozart <jamisoncozart@gmail.com>
2020-05-08 16:41:48 -0700

    Update authorization to redirect to Signin

--- src/Components/App.js
+++ src/Components/App.js
@@ -5,25 +5,27 @@ import Signin from './Signin';
 import Header from './Header';
 import FooterNav from './FooterNav';
 import Body from './Body/Body';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link, useHistory } from 'react-router-dom';
 import { withFirestore, isLoaded } from 'react-redux-firebase';
 import '../App.css';
 
 let App = props => {
 
   const [authToggle, setAuthToggle] = useState(false);
+  const history = useHistory();
 
   console.log('App rendered 😀');
 
-  // const auth = props.firebase.auth();
+  const auth = props.firebase.auth();
   let authContent = null;
-  // if(!isLoaded(auth)) {
-  //   authContent = <h1>Loading...</h1>;
-  // } else if((isLoaded(auth)) && (auth.currentUser == null)) {
-  //   authContent = <h1>You must be signed in to access this content! <Link to='/signin'>Sign In</Link></h1>;
-  // } else if((isLoaded(auth)) && (auth.currentUser != null)) {
+  if(!isLoaded(auth)) {
+    authContent = <h1>Loading...</h1>;
+  } else if((isLoaded(auth)) && (auth.currentUser == null)) {
+    // authContent = <h1>You must be signed in to access this content! <Link to='/signin'>Sign In</Link></h1>;
+    history.push('/signin');
+  } else if((isLoaded(auth)) && (auth.currentUser != null)) {
     authContent = <Body />;
-  // }
+  }
 
   return(
     <Router>
@@ -35,8 +37,7 @@ let App = props => {
           <Signin handleSignIn={setAuthToggle}/>
         </Route>
         <Route path='/'>
-          <Header handleSignOut={setAuthToggle}/>
-          {/* <p>{auth.currentUser === null ? '' : ('hello ' + auth.currentUser.displayName + ' !')}</p> */}
+          <Header currenUser={auth.currentUser !== null ? auth.currentUser.displayName : ''} handleSignOut={setAuthToggle}/>
           {authContent}   
           <FooterNav />
         </Route>
