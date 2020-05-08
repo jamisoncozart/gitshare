6c5e941c8e4958b51eeea593884eac81f56fa390
Jamison Cozart <jamisoncozart@gmail.com>
2020-05-08 16:08:50 -0700

    Add NewPostForm inputs and styling

--- src/Components/Body/NewPostForm.js
+++ src/Components/Body/NewPostForm.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 const NewPostForm = props => {
   return (
-    <h2>NewPostForm</h2>
+    <div className='newPostFormDiv'>
+      <form className='siginForm'>
+        <h2>New Post</h2>
+        <input name='PostTitle' type='text' placeholder='Title' />
+        <textarea name='PostDescription' type='text' placeholder='Description' rows='4' />
+        <button className='submitButton' type='submit'>Submit</button>
+      </form>
+    </div>
   );
 }
 
