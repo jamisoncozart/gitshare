0b129769708bb713c695937471981516e50f88d2
Jamison Cozart <jamisoncozart@gmail.com>
2020-05-10 18:03:15 -0700

    Show user what tag they are filtering by, allow user to clear filter

--- src/Components/Body/Feed/Feed.js
+++ src/Components/Body/Feed/Feed.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useSelector } from 'react-redux'
 import { useFirestoreConnect, isLoaded, isEmpty } from 'react-redux-firebase';
 import Post from './Post';
+import Tag from './Tag';
 
 const Feed = props => {
   useFirestoreConnect([
@@ -31,19 +32,29 @@ const Feed = props => {
   if(isLoaded(posts)) {
     if(!viewingDetails) {
       return (
-        <div className='postsDiv'>
-          {posts.map((post, index) => {
-            return (
-              <Post 
-                showDetails={false}
-                handleShowingPostDetails={handleShowingPostDetails}
-                handleClickingBack={null}
-                handleFilterTag={handleFilterTag}
-                post={post}
-                key={index}/>
-            );
-          })}
-        </div>
+        <React.Fragment>
+          {tagFiltering ? 
+            <div className='filterTag'>
+              <div className='filterTagHeader'>
+                <p>Filtering by: </p>
+                <Tag name={filterTag} />
+              </div>
+              <button onClick={() => setTagFiltering(false)}>Clear</button>
+            </div> : null}
+          <div className='postsDiv'>
+            {posts.map((post, index) => {
+              return (
+                <Post 
+                  showDetails={false}
+                  handleShowingPostDetails={handleShowingPostDetails}
+                  handleClickingBack={null}
+                  handleFilterTag={handleFilterTag}
+                  post={post}
+                  key={index}/>
+              );
+            })}
+          </div>
+        </React.Fragment>
       )
     } else {
       return (
