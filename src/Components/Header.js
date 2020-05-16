6d01bc335e5d8fac1b05e2bbb38b031e8a26fbea
Jamison Cozart <jamisoncozart@gmail.com>
2020-05-15 18:33:05 -0700

    Toggle side menu button themes on activation

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,6 +7,10 @@ import { isLoaded } from 'react-redux-firebase';
 function Header(props) {
   const auth = firebase.auth();
   const history = useHistory();
+  const [topActiveTheme, setTopActiveTheme] = useState(false);
+  const [newActiveTheme, setNewActiveTheme] = useState(false);
+  const [followActiveTheme, setFollowActiveTheme] = useState(false);
+  const [darkTheme, setDarkTheme] = useState(false);
 
   function doSignOut() {
     auth.signOut().then(function() {
@@ -22,16 +26,19 @@ function Header(props) {
   }
 
   const [navOpen, setNavOpen] = useState(false);
-  const openNav = () => {
-    setNavOpen(true);
-    // document.getElementsByClassName('sideNav')[0].style.width = '100px';
-    // document.body.style.opacity = '0.6';
+
+  const handleTogglingSideButton = button => {
+    if(button === 'top') {
+      setTopActiveTheme(!topActiveTheme);
+    } else if(button === 'new') {
+      setNewActiveTheme(!newActiveTheme);
+    } else if(button === 'follow') {
+      setFollowActiveTheme(!followActiveTheme);
+    }
+    props.handlePressingSidebarButton(button);
   }
-  const closeNav = () => {
-    setNavOpen(false);
-    // document.getElementsByClassName('sideNav')[0].style.width = '0';
-    // document.body.style.opacity = '1';
-  }
+
+
 
   return (
     <div className='header'>
@@ -39,7 +46,7 @@ function Header(props) {
       <div className='headerRight'>
         <p className='displayName'>{currentUser}</p>
         <button 
-          className='sidebarButton' 
+          className={'sidebarButton'} 
           onClick={() => setNavOpen(true)}>
             <img src='https://www.contentformula.com/blog/wp-content/uploads/2016/06/hamburger-menu.png'/>
         </button>
@@ -48,25 +55,31 @@ function Header(props) {
         <button 
           onClick={() => setNavOpen(false)} 
           className="closebtn">
-            <img src='https://cdn.iconscout.com/icon/free/png-256/chevron-25-433513.png'/>
+            <img src='https://cdn.iconscout.com/icon/free/png-256/chevron-27-433515.png'/>
         </button>
         <button 
-          onClick={() => props.handlePressingSidebarButton('top')}
-          className='sideNavButton'>
+          onClick={() => handleTogglingSideButton('top')}
+          className={topActiveTheme ? 'activeSideButton' : 'sideNavButton'}>
             <img src='https://icons.iconarchive.com/icons/google/noto-emoji-travel-places/256/42697-fire-icon.png'/>
             Top
         </button>
         <button 
-          onClick={() => props.handlePressingSidebarButton('new')}
-          className='sideNavButton'>
+          onClick={() => handleTogglingSideButton('new')}
+          className={newActiveTheme ? 'activeSideButton' : 'sideNavButton'}>
             <img src='https://icons.iconarchive.com/icons/google/noto-emoji-activities/256/52705-sparkles-icon.png'/>
             New
         </button>
         <button 
-          onClick={() => props.handlePressingSidebarButton('follow')}
-          className='sideNavButton'>
+          onClick={() => handleTogglingSideButton('follow')}
+          className={followActiveTheme ? 'activeSideButton' : 'sideNavButton'}>
             <img src='https://tribemobile.co/wp-content/uploads/2016/06/connect-icon.png'/>
             Follows
+        </button>
+        <button 
+          onClick={() => setDarkTheme(!darkTheme)}
+          className={darkTheme ? 'darkActiveSideButton' : 'sideNavButton'}>
+            <img src={darkTheme ? 'https://i2.wp.com/hostasonthebluff.com/wp-content/uploads/2017/10/Sun-Icon.png?fit=256%2C256&ssl=1' : 'https://cdn.iconscout.com/icon/free/png-256/half-moon-1767806-1502386.png'}/>
+            Theme
         </button>
         <button 
           className='sideNavButton' 
@@ -75,7 +88,7 @@ function Header(props) {
             Logout
         </button>
       </div>
-      <div className={navOpen ? 'darknessStyle' : 'nothing'}></div>
+      <div onClick={() => setNavOpen(false)} className={navOpen ? 'darknessStyle' : 'nothing'}></div>
     </div>
   );
 }
